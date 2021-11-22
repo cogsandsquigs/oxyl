188fe15995c0ccc1d9c54d79b84c70dff79a7313
Ian Pratt <ianjdpratt@gmail.com>
2021-11-21 19:56:24 -0600

    added functions for executing operations on types

--- std/types.go
+++ std/types.go
@@ -6,7 +6,7 @@ import "fmt"
 type Type struct {
 	name        string
 	cmpoptypes  map[Operator][]string
-	execopfuncs map[Operator]func(l, r *Value) *Value
+	execopfuncs map[Operator]func(l, r *Value) (interface{}, string)
 }
 
 // Name returns the name of the type
@@ -16,7 +16,7 @@ func (t *Type) Name() string {
 
 // CmpOpType stands for Compatible Operator and Type. Checks to see if type and operator can be used together.
 // The assumption here is that `t` is on the left side, which sets the precedent for the operation.
-func (t *Type) CmpOpType(op Operator, typ Type) bool {
+func (t *Type) CmpOpType(op Operator, typ *Type) bool {
 	typeNameList, ok := t.cmpoptypes[op]
 	if !ok {
 		return ok
@@ -31,16 +31,16 @@ func (t *Type) CmpOpType(op Operator, typ Type) bool {
 }
 
 // ExecuteOp executes an operation on a left and right value, with the assumption that the left value has the same type as the type being used here.
-func (t *Type) ExecuteOp(op Operator, l, r *Value) (*Value, error) {
+func (t *Type) ExecuteOp(op Operator, l, r *Value) (interface{}, string, error) {
 	if _, ok := t.cmpoptypes[op]; !ok {
-		return nil, fmt.Errorf("execution error: operator " + string(op) + " does not exist")
+		return nil, "None", fmt.Errorf("execution error: operator " + string(op) + " does not exist")
 	} else if l.typ.name != t.name {
-		return nil, fmt.Errorf("execution error: left operand of type " + l.typ.name + " is not equal to expected type " + t.name)
+		return nil, "None", fmt.Errorf("execution error: left operand of type " + l.typ.name + " is not equal to expected type " + t.name)
 	} else if !t.CmpOpType(op, r.typ) {
-		return nil, fmt.Errorf("execution error: the operator " + string(op) + " is not defined for types " + t.name + " and " + r.typ.name)
+		return nil, "None", fmt.Errorf("execution error: the operator " + string(op) + " is not defined for types " + t.name + " and " + r.typ.name)
 	}
-
-	return t.execopfuncs[op](l, r), nil
+	i, s := t.execopfuncs[op](l, r)
+	return i, s, nil
 }
 
 // Int is the integer type
@@ -52,6 +52,48 @@ var Int = Type{
 		MUL: {"Int", "Float"},
 		DIV: {"Int", "Float"},
 	},
+	execopfuncs: map[Operator]func(l, r *Value) (interface{}, string){
+		ADD: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(int64) + r.val.(int64), "Int"
+			case "Float":
+				return float64(l.val.(int64)) + l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		SUB: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(int64) - r.val.(int64), "Int"
+			case "Float":
+				return float64(l.val.(int64)) - l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		MUL: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(int64) * r.val.(int64), "Int"
+			case "Float":
+				return float64(l.val.(int64)) * l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		DIV: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(int64) / r.val.(int64), "Int"
+			case "Float":
+				return float64(l.val.(int64)) / l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+	},
 }
 
 // Float is the floating point number type
@@ -62,5 +104,47 @@ var Float = Type{
 		SUB: {"Int", "Float"},
 		MUL: {"Int", "Float"},
 		DIV: {"Int", "Float"},
+	},
+	execopfuncs: map[Operator]func(l, r *Value) (interface{}, string){
+		ADD: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(float64) + float64(r.val.(int64)), "Int"
+			case "Float":
+				return l.val.(float64) + l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		SUB: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(float64) - float64(r.val.(int64)), "Int"
+			case "Float":
+				return l.val.(float64) - l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		MUL: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(float64) * float64(r.val.(int64)), "Int"
+			case "Float":
+				return l.val.(float64) * l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
+		DIV: func(l, r *Value) (interface{}, string) {
+			switch r.typ.name {
+			case "Int":
+				return l.val.(float64) / float64(r.val.(int64)), "Int"
+			case "Float":
+				return l.val.(float64) / l.val.(float64), "Float"
+			default:
+				return nil, "None"
+			}
+		},
 	},
 }
--- std/values.go
+++ std/values.go
@@ -2,17 +2,17 @@ package std
 
 // Value defines the structure for the values in Oxyl
 type Value struct {
-	typ Type
+	typ *Type
 	val interface{}
 }
 
 // NewValue creates a new value with a type and an interface
-func NewValue(typ Type, val interface{}) *Value {
+func NewValue(typ *Type, val interface{}) *Value {
 	return &Value{typ, val}
 }
 
 // gets the value's type
-func (v *Value) Type() Type {
+func (v *Value) Type() *Type {
 	return v.typ
 }
 
