d1133fa9ad8e4b8954b6a2d0ccecfcef7fb2586a
Ian Pratt <thingcoder1@gmail.com>
2022-03-30 15:01:46 +0000

    changes

--- shell.nix
+++ shell.nix
@@ -1,8 +1,19 @@
+#{ pkgs ? import <nixpkgs> {} }:
+# 
+#pkgs.mkShell {
+#  name = "go-env";
+#  buildInputs = [
+#    pkgs.go
+#  ];
+#}
+
 { pkgs ? import <nixpkgs> {} }:
  
-pkgs.mkShell {
-  name = "go-env";
-  buildInputs = [
-    pkgs.go
-  ];
+pkgs.mkShell rec {
+  name = "env";
+
+  shellHook = ''
+	nix-channel --update
+    nix-env -iA nixpkgs.go_1_18
+  '';
 }
