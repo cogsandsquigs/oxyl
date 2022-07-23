2d294ced0c46692fc215d409f878f946ba803693
Ian Pratt <ianjdpratt@gmail.com>
2022-07-23 20:34:15 +0000

    changes

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,13 @@
 pub trait Node {
-    fn children() -> Vec<Box<Self>>;
+    fn children(&self) -> Vec<Box<dyn Node>>
+    where
+        Self: Sized;
+}
+
+pub struct Literal {}
+
+impl Node for Literal {
+    fn children(&self) -> Vec<Box<dyn Node>> {
+        return vec![];
+    }
 }
